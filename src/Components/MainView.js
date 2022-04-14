dd5dd3d4a1ea77086e1548907569d04d32ed0733
bishop254 <karanjachiuri@gmail.com>
2022-04-14 22:14:54 +0300

    Created a new component.
    Tested the GraphQL query and checked if data is being returned.

--- src/Components/MainView.js
+++ src/Components/MainView.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const BOOK_QUERY = gql`
+  query GetBook {
+    book {
+      author
+      title
+      pages {
+        content
+        tokens {
+          value
+        }
+      }
+    }
+  }
+`;
 
 const MainView = () => {
-  return (
-    <div>MainView</div>
-  )
-}
+  const { data, loading, error } = useQuery(BOOK_QUERY); //data is the object returned from our endpoint.
+  const [currentPage, setCurrentPage] = useState(1);
 
-export default MainView
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div className="main">
+      <div className="header">
+        <p>{data.book.author}</p>
+        <p>{data.book.title}</p>
+      </div>
+      {data.book.pages.map((page) => (
+        <div className="content">
+            {page.content}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MainView;
