ab9afd3ed1f648583dfb990595ce7173c6ae0c8f
bishop254 <karanjachiuri@gmail.com>
2022-04-15 08:03:46 +0300

    MainView Component was updated to display two pages per view.
    Also allow the view to change based on which page number is clicked.
    
    Linked MainView to both Pagination and Content Components.
    
    Styled our page numbers to display in a single line.

--- src/Components/MainView.js
+++ src/Components/MainView.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import "./MainView.css";
+import Content from "./Content";
+import Pagination from "./Pagination";
 
 const BOOK_QUERY = gql`
   query GetBook {
@@ -20,10 +22,20 @@ const BOOK_QUERY = gql`
 const MainView = () => {
   const { data, loading, error } = useQuery(BOOK_QUERY); //data is the object returned from our endpoint.
   const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 2;
 
   if (loading) return <div>Loading...</div>;
 
   if (error) return <div>{error}</div>;
+
+  const lastPageIndex = currentPage * pageSize;
+  const firstPageIndex = lastPageIndex - pageSize;
+  const currPages = [...data.book.pages].slice(firstPageIndex, lastPageIndex); //Updates pages displayed to the user.
+
+  // Updates the view(page content) when user navigates to another page.
+  const paginate = (pageNo) => {
+    setCurrentPage(pageNo);
+  };
 
   //Toggle between viewing the page and viewing the token value.
   const goBack = () => {
@@ -43,12 +55,19 @@ const MainView = () => {
           <p>{data.book.author}</p>
           <p>{data.book.title}</p>
         </div>
-        {data.book.pages.map((page) => (
-          <div className="content">{page.content}</div>
+        {currPages.map((page) => (
+          <>
+            <Content item={page.content} token={page.tokens} />
+          </>
         ))}
+        <Pagination
+          pagesToDisplay={pageSize}
+          totalPages={data.book.pages.length}
+          paginate={paginate}
+        />
       </div>
       <div className="token unactive">
-        <button onClick={goBack}>Exit</button>
+        <button onClick={goBack}>Go Back</button>
       </div>
     </>
   );
