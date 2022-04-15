910890149b0fe24d0e8786f03bcd433406467acb
bishop254 <karanjachiuri@gmail.com>
2022-04-15 07:32:49 +0300

    Edited our Content components allowing it to take props.
    It displays the sentences in a page and listen for the click event
    
    Updated MainView Component. Created a new <div> to hold the token value.

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Content = () => {
-  return (
-    <div>Content</div>
-  )
-}
+const Content = ({ item, token }) => {
+  let arr = item.split(" ");
 
-export default Content
+  //Display token value on user click event
+  const handleClick = (event) => {
+    let clickedWord = event.target.innerHTML.toLowerCase();
+
+    let tokenArr = token.filter((tk) => clickedWord.includes(tk.value)); // Allows us to match a word that is beside a punctuation mark(", ., ?)
+
+    //Display the token value on the current user view.
+    let mainDiv = document.querySelector(".main");
+    let tokenDiv = document.querySelector(".token");
+    let newElement = document.createElement("div");
+
+    newElement.classList.add("tokenInfo"); //Gives it a className which allows us to remove this element when needed.
+    newElement.innerHTML = tokenArr[0].value; //tokenArr may return an array with more than one element.
+
+    mainDiv.classList.add("unactive");
+    tokenDiv.classList.remove("unactive");
+
+    tokenDiv.appendChild(newElement);
+  };
+
+  return arr.map((word) => (
+    <>
+      <span onClick={(e) => handleClick(e)}>{word}</span>
+      <span> </span>
+    </>
+  ));
+};
+
+export default Content;
--- src/Components/MainView.js
+++ src/Components/MainView.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
+import "./MainView.css";
 
 const BOOK_QUERY = gql`
   query GetBook {
@@ -24,18 +25,32 @@ const MainView = () => {
 
   if (error) return <div>{error}</div>;
 
+  //Toggle between viewing the page and viewing the token value.
+  const goBack = () => {
+    let mainDiv = document.querySelector(".main");
+    let tokenDiv = document.querySelector(".token");
+    let tokenInfo = document.querySelector(".tokenInfo");
+
+    tokenDiv.removeChild(tokenInfo);
+    tokenDiv.classList.add("unactive");
+    mainDiv.classList.remove("unactive");
+  };
+
   return (
-    <div className="main">
-      <div className="header">
-        <p>{data.book.author}</p>
-        <p>{data.book.title}</p>
-      </div>
-      {data.book.pages.map((page) => (
-        <div className="content">
-            {page.content}
+    <>
+      <div className="main">
+        <div className="header">
+          <p>{data.book.author}</p>
+          <p>{data.book.title}</p>
         </div>
-      ))}
-    </div>
+        {data.book.pages.map((page) => (
+          <div className="content">{page.content}</div>
+        ))}
+      </div>
+      <div className="token unactive">
+        <button onClick={goBack}>Exit</button>
+      </div>
+    </>
   );
 };
 
